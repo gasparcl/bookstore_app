d6ec1687d7eba69107c7d3ff436527a4cdeea369
Lucas Gaspar de Carvalho <lucas@ecosis.com.br>
2022-11-09 15:04:26 -0300

    Feat: Implemented Devise API JWT - BACKEND

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -5,12 +5,17 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # URL or IP address from the frontend client 
+    # development:
+    origins "http://localhost:5173"
+    # production:
+    # origins "production URL or IP Address"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: %w[Authorization Uid]
+  end
+end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users,
+  controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
+  get "/member-data", to: "members#show"
+
+  namespace :api do
+    namespace :v1 do
+      resources :books
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
